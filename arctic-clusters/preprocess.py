a3937d1e57dbb85e284cfc1ea2a9a7b176b85444
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2025-01-30 16:11:29 +0000

    clean up preprocessing and run scripts

--- arctic-clusters/preprocess.py
+++ arctic-clusters/preprocess.py
@@ -1,50 +1,50 @@
 import os.path
+import glob
+import logging
 
-import numpy as np
-import netCDF4
+import fabmos.input.cluster
+import pygetm.input.glodap
 
-import fabmos
-
-GLODAP_FILE = "glodap.nc"
-
-assert os.path.isfile(
-    GLODAP_FILE
-), f"First download GLODAP with `python -m pygetm.input.glodap {GLODAP_FILE}`"
-
-with netCDF4.Dataset("smoothed_ecoregion_6_iteration.nc") as nc:
-    lon = nc["longitude"][:, :]
-    lat = nc["latitude"][:, :]
-
-
-def get_initial_conditions(lon: np.ndarray, lat: np.ndarray):
-    with netCDF4.Dataset(GLODAP_FILE) as nc:
-        target_variables = [n for n, v in nc.variables.items() if v.ndim == 3]
-    lat_glodap = np.minimum(lat, 87.5)
-    with netCDF4.Dataset("glodap_ip.nc", "w") as ncbath:
-        for name in target_variables:
-            values = fabmos.input.from_nc(GLODAP_FILE, name)
-            values = fabmos.input.limit_region(
-                values, -180.0, 180.0, -80.0, 89.5, periodic_lon=True
-            )
-            if not ncbath.variables:
-                ncbath.createDimension("x", lon.shape[1])
-                ncbath.createDimension("y", lon.shape[0])
-                ncbath.createVariable("lon", float, ("y", "x"))[:, :] = lon
-                ncbath.createVariable("lat", float, ("y", "x"))[:, :] = lat
-                glodap_depth = values.coords["Depth"]
-                ncbath.createDimension("depth", len(glodap_depth))
-                ncdepth = ncbath.createVariable("depth", float, ("depth",))
-                ncdepth[:] = glodap_depth
-                ncdepth.positive = glodap_depth.attrs["positive"]
-            values_ip = fabmos.input.horizontal_interpolation(values, lon, lat_glodap)
-            values_ip = np.asarray(values_ip)
-            ncvar = ncbath.createVariable(
-                name, float, ("depth", "y", "x"), fill_value=-1
-            )
-            ncvar.long_name = values.long_name
-            ncvar.units = values.units
-            ncvar[:, :, :] = values_ip
-
+import run
+from download import TARGET_DIR as DATA_DIR
 
 if __name__ == "__main__":
-    get_initial_conditions(lon, lat)
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger()
+
+    domain = run.get_domain(cluster=True)
+
+    # Interpolate GLODAP to the original model grid, but wait with averaging
+    # over clusters until runtime, as we need the vertical layer distribution
+    pygetm.input.glodap.regrid(
+        os.path.join(DATA_DIR, "glodap.nc"),
+        domain.full_lon,
+        domain.full_lat,
+        "glodap_ip.nc",
+        clamp=True,
+        logger=logger,
+    )
+
+    # Calculate transport between clusters from CMEMS velocities
+    FLOW_FILE = os.path.join(DATA_DIR, "cmems", "flow.nc")
+    u = fabmos.input.from_nc(FLOW_FILE, "vxo")
+    v = fabmos.input.from_nc(FLOW_FILE, "vyo")
+    fabmos.input.cluster.get_connectivity(
+        domain, u, v, "connections.nc", periodic_lon=True, logger=logger
+    )
+
+    # Average ERA5 per cluster (special treatment of wind components u10 and
+    # v10 to preserve average wind speed)
+    ERA_FILES = os.path.join(DATA_DIR, "era5", "era5_????.nc")
+    for infile in glob.glob(ERA_FILES):
+        outfile = f"{os.path.basename(infile)[:-3]}.nc"
+        logger.info(f"Averaging {infile} and saving to {outfile}...")
+        fabmos.input.cluster.average(
+            domain,
+            infile,
+            outfile,
+            chunksize=24 * 20,
+            averager=fabmos.input.cluster.average_uv,
+            periodic_lon=True,
+            logger=logger,
+        )
